a840b4a4f28b2e0025d4611ececdf58aedefb25e
Rayan <rayanwork2014@gmail.com>
2022-12-15 19:52:40 +0200

    add media url

--- src/models/Others/Media/media.ts
+++ src/models/Others/Media/media.ts
@@ -4,15 +4,15 @@ export interface Media {
     id?: number
     model_id : unknown
     model_type: string
-    relative_path?: string 
-    is_featured?: string  
+    relative_path?: string
+    is_featured?: string
   }
   class MediaConsts extends BaseConsts {
     static readonly EMPLOYEE_MODEL_ROUTE = "App\\Domain\\Employee\\Entity\\Employee";
     static readonly CONTRACTOR_MODEL_ROUTE = "App\\Domain\\Contractor\\Entity\\Contractor";
     static readonly CUSTOMER_MODEL_ROUTE = "App\\Domain\\CRM\\Entity\\Customer";
-    static readonly MEDIA_BASE_URL = 'http://127.0.0.1:8000/storage/';
-    
+    static readonly MEDIA_BASE_URL = import.meta.env.VITE_MEDIA_BASE_URL;
+
 }
 
 export {MediaConsts}
