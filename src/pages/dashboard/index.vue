8cfa6db48391e924db57f6c985582084450e533a
Rayan <rayanwork2014@gmail.com>
2023-01-18 19:03:28 +0200

    fix

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
-import { useHead } from '@vueuse/head'
-import { useViewWrapper } from '/@src/stores/viewWrapper'
-import { useI18n } from 'vue-i18n'
+<script lang="ts" setup>
+import {useHead} from '@vueuse/head'
+import {useViewWrapper} from '/@src/stores/viewWrapper'
+import {useI18n} from 'vue-i18n'
 
-const { t } = useI18n()
+const {t} = useI18n()
 const route = useRoute()
 const router = useRouter()
 const pageTitle = ref('')
 const viewWrapper = useViewWrapper()
-viewWrapper.setPageTitle(t('main_dashboar'))
+viewWrapper.setPageTitle(t('main_dashboard'))
 
 
 useHead({
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,15 @@
-<route lang="json">
-  {
-  "meta": {
-  "requiresAuth": true
-  }
-  }
-  </route>
-  <script setup lang="ts">
-  const route = useRoute()
-  const renderKey = ref(0)
-  onBeforeMount(() => {
-    renderKey.value++
-  })
-  onMounted(() => {
-    renderKey.value++
-  })
-  
-  
-  </script>
-  
-  <template>
+<script lang="ts" setup>
+import {useHead} from '@vueuse/head'
 
-  </template>
-  
+useHead({
+  title: 'Home Page',
+})
+const router = useRouter();
+router.push({
+  path: '/dashboard/'
+})
+</script>
+
+<template>
+
+</template>
