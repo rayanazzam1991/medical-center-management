5a9e90bb2a305317601edd8d94efa59fdfbe6299
Ranim <106822042+RanimHelamiah@users.noreply.github.com>
2023-01-23 11:07:03 +0200

    some edit

--- src/services/Contractor/WalletMovement/walletMovementService.ts
+++ src/services/Contractor/WalletMovement/walletMovementService.ts
@@ -2,18 +2,9 @@ import { CreateWalletMovement, defaultWalletMovement, WalletMovement } from "/@s
 import { useWalletMovement } from "/@src/stores/Contractor/WalletMovement/walletMovementStore"
 import { Pagination } from "/@src/utils/response"
 
-export async function addWalletMovement(
-    walletMovementData: CreateWalletMovement,
-) {
-    const newWalletMovementData: CreateWalletMovement = {
-        type: walletMovementData.type,
-        action: walletMovementData.action,
-        total: walletMovementData.total,
-        wallet_id: walletMovementData.wallet_id,
-        note: walletMovementData.note,
-    }
+export async function addWalletMovement(walletMovementData: CreateWalletMovement) {
     const walletMovementResponse = useWalletMovement()
-    let walletMovement: WalletMovement = await walletMovementResponse.addWalletMovementStore(newWalletMovementData) ?? defaultWalletMovement
+    let walletMovement: WalletMovement = await walletMovementResponse.addWalletMovementStore(walletMovementData) ?? defaultWalletMovement
     let success: boolean = walletMovementResponse.success ?? false
     let error_code: string = walletMovementResponse.error_code ?? ''
     let message: string = walletMovementResponse.message ?? ''
