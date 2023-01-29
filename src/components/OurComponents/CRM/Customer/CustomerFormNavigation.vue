885e1b3cf700818ac7a57e526bbf491874be2ca2
Rayan <rayanwork2014@gmail.com>
2023-01-29 15:36:50 +0200

    add staging env

--- src/components/OurComponents/CRM/Customer/CustomerFormNavigation.vue
+++ src/components/OurComponents/CRM/Customer/CustomerFormNavigation.vue
@@ -1,27 +1,25 @@
-<script setup lang="ts">
-import { useI18n } from 'vue-i18n';
-import { useDropdown } from '/@src/composable/useDropdown'
-import { useCustomerForm } from '/@src/stores/CRM/Customer/customerFormSteps'
-import { useDarkmode } from '/@src/stores/darkmode'
-import { onceImageErrored } from '/@src/utils/via-placeholder'
+<script lang="ts" setup>
+import {useI18n} from 'vue-i18n';
+import {useDropdown} from '/@src/composable/useDropdown'
+import {useCustomerForm} from '/@src/stores/CRM/Customer/customerFormSteps'
+import {useDarkmode} from '/@src/stores/darkmode'
 
 const darkmode = useDarkmode()
 
 const dropdownElement = ref<HTMLElement>()
 const dropdown = useDropdown(dropdownElement)
-const {t,locale} = useI18n()
+const {t, locale} = useI18n()
 const customerForm = useCustomerForm()
-const locale = useStorage('locale','ar')
-const iconArrow = locale.value =="ar" ? "lnir-arrow-right":"lnir-arrow-left"
+const iconArrow = locale.value == "ar" ? "lnir-arrow-right" : "lnir-arrow-left"
 </script>
 
 <template>
   <nav class="wizard-navigation">
     <div class="">
       <span class="title-wrap">
-        <VButton class="navbar-item is-wizard-title" :icon="`lnir ${iconArrow} rem-100`" to="/customer" darkOutlined
-          color="white">
-          {{t('customer.form.back_button')}}
+        <VButton :icon="`lnir ${iconArrow} rem-100`" class="navbar-item is-wizard-title" color="white" darkOutlined
+                 to="/customer">
+          {{ t('customer.form.back_button') }}
         </VButton>
       </span>
     </div>
@@ -33,7 +31,7 @@ const iconArrow = locale.value =="ar" ? "lnir-arrow-right":"lnir-arrow-left"
     <div class="navbar-item is-dark-mode">
       <div class="navbar-icon">
         <label class="dark-mode">
-          <input data-cy="dark-mode-toggle" type="checkbox" :checked="!darkmode.isDark" @change="darkmode.onChange" />
+          <input :checked="!darkmode.isDark" data-cy="dark-mode-toggle" type="checkbox" @change="darkmode.onChange"/>
           <span></span>
         </label>
       </div>
