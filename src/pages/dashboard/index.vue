a35b7bb1a51385e634da5a315b433df677d5e3ee
Tareq Aldarwish <tarekdar1@hotmail.com>
2023-05-09 17:06:12 +0300

    cashier dashboard

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -23,10 +23,8 @@ const userRole = ref()
 if (user.value.default_role) {
   userRole.value = user.value.default_role.name
 } else {
-
-  userRole.value = user?.value?.roles?.at(0)?.name as 'Admin' | 'Accountant' | 'Human_Resources' | 'Inventory' | 'Service_Provider' | 'Receptionist' | 'Analyst'
+  userRole.value = user?.value?.roles?.at(0)?.name as 'Admin' | 'Accountant' | 'Human_Resources' | 'Inventory' | 'Service_Provider' | 'Receptionist' | 'Analyst' | 'Cashier'
 }
-
 useHead({
   title: t('dashboards.title'),
 })
